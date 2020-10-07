f69a91fa2105169193474d9e9cbd690c485b7874
jhen <developer@jhen.me>
2020-10-07 11:09:39 +0800

    Use graphql_scalar derive

--- juniper/src/integrations/json.rs
+++ juniper/src/integrations/json.rs
@@ -2,30 +2,32 @@ use serde_json::Value as JsonValue;
 
 use crate::{
     parser::{ParseError, ScalarToken, Token},
-    value::{ParseScalarResult, ParseScalarValue},
+    value::{ParseScalarResult},
     Value,
 };
 
-graphql_scalar!(JsonValue as "JsonString" where Scalar = <S> {
-    description: "JSON serialized as a string"
-
-    resolve(&self) -> Value {
+#[crate::graphql_scalar(description = "JSON serialized as a string")]
+impl<S> GraphQLScalar for JsonValue
+where
+    S: ScalarValue,
+{
+    fn resolve(&self) -> Value {
         Value::scalar(self.to_string())
     }
 
-    from_input_value(v: &InputValue) -> Option<JsonValue> {
-        v.as_scalar_value::<String>()
+    fn from_input_value(v: &InputValue) -> Option<JsonValue> {
+        v.as_string_value()
          .and_then(|s| serde_json::from_str(s).ok())
     }
 
-    from_str<'a>(value: ScalarToken<'a>) -> ParseScalarResult<'a, S> {
+    fn from_str<'a>(value: ScalarToken<'a>) -> ParseScalarResult<'a, S> {
         if let ScalarToken::String(value) = value {
             Ok(S::from(value.to_owned()))
         } else {
             Err(ParseError::UnexpectedToken(Token::Scalar(value)))
         }
     }
-});
+}
 
 #[cfg(test)]
 mod test {
